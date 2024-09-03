a30f28184d7877cd97e2b76a8c0be24ed29a0498
oficialph <pedroh2006.14@gmail.com>
2024-09-03 15:05:30 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,7 @@
+numero_de_cavalos = int(input("Digite o número de cavalos: "))
+
+custo_ferraduras = 320.00
+
+custo_total = numero_de_cavalos * custo_ferraduras
+
+print(f"O valor total para trocar todas as quatro ferraduras dos cavalos é: R${custo_total:.2f}")
